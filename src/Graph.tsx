8b4bd457b77feaa2163b54e67e21272b8cc82a36
Aya <aya@Ayas-MacBook-Air.local>
2023-01-01 18:52:01 +0300

    fixed graph not showing

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -52,16 +52,16 @@ class Graph extends Component<IProps, {}> {
       // Add more Perspective configurations here.
       elem.load(this.table);
       elem.setAttribute("view", "y_line");
-      elem.setAttribute("column-pivote", '["stock"]');
-      elem.setAttribute("row-pivote", '["timestamp"]');
+      elem.setAttribute("column-pivotes", '["stock"]');
+      elem.setAttribute("row_pivotes", '["timestamp"]');
       elem.setAttribute("columns", '["top_ask_price"]');
       elem.setAttribute(
         "aggregates",
         `{
-        "stock","distinct count",
-        "top_ask_price","avg",
-        "top_bid_price","avg",
-        "timestamp","distinct count"
+        "stock":"distinct count",
+        "top_ask_price":"avg",
+        "top_bid_price":"avg",
+        "timestamp":"distinct count"
       }
       `
       );
